2e1dd8e1241675b0bb01d72396a46e7eb72b3eb2
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-05-13 07:09:22 +0700

    Update Capitalize First Letters.js

--- Capitalize First Letters.js
+++ Capitalize First Letters.js
@@ -1,35 +1,30 @@
-// We will assume the worst case scenario where users will input some capital letters
-// So we will make sure to convert the input to lowercase first.
+// Convert first letter in every word to capital letters.
+// Let's make sure the input is right
 
-// We will assume the worst case scenario where users will input some capital letters
-// So we will make sure to convert the input to lowercase first.
 
+
+// SOLUTION 1 Using a normal for loop
+// Let's assume people put in random words with some of them are capital letters 
 function convertLetter(input) {
+let words = input.toLowerCase().split(" ")
 
-    let words = input.toLowerCase().split(" ")   
-    //Loop through each letter
-    // Convert that to array ?
-    
-    // Now slice out the first letter in each word + the rest of the wordds
-    
-    for(let i = 0; i < words.length; i++){
-      words[i][0].toUpperCase() + words[i].slice(1)
-      }
-      return words.join(" ")
-    }
-    
-    
-    
-    
-    // We can use ES6 Map function to achieve the same result 
-    // This code can be shorten even further
-    
-    function convertLetter2(str) {
-      let titled = str.toLowerCase().split(" ").map((element) => {
-        return element[0].toUpperCase() + elem.slice(1);
-      })
-    }
-    convertLetter("just checking");
-    
-    
-    
+// If we set the variable to words only. It will only pick the first word and neglect the rest
+// So we must use words[i]
+for(let i = 0 ; i<words.length; i++) {
+ words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+}
+return words.join(" ")
+}
+
+convertLetter("just waitting for the next")
+
+// SOLUTION 2 Using ES6 and map function
+convertLetter2 = (input) => {
+let words = input.toLowerCase().split(" ").map(element => element[0].toUpperCase() + element.slice(1)) 
+ return words.join(" ")
+  
+}
+
+convertLetter2("testing the solution")
+
+// Performance why I don't think it makes a difference because they are both utilizing array so the operation stil 0(n)
